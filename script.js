804a8c755f7a6f9be38203d931b0258ee05b2624
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-08-07 16:04:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 //your JS code here. If required.
-function displayBrowserInfo() {
-      // Get the browser name and version
-      const browserName = navigator.appName;
-      const version = navigator.appVersion;
+document.addEventListener("DOMContentLoaded", function () {
+  const browserInfoDiv = document.getElementById("browser-info");
+  const browserName = getBrowserName();
+  const browserVersion = getBrowserVersion();
 
-      // Display the browser information on the page
-      const browserInfo = document.getElementById("browser-info");
-      browserInfo.textContent = "You are using " + browserName + " version " + version;
-    }
+  const message = "You are using " + browserName + " version " + browserVersion;
+  browserInfoDiv.textContent = message;
+});
+
+function getBrowserName() {
+  const userAgent = navigator.userAgent;
+  if (userAgent.includes("Firefox")) {
+    return "Firefox";
+  } else if (userAgent.includes("Chrome")) {
+    return "Chrome";
+  } else if (userAgent.includes("Safari")) {
+    return "Safari";
+  } else if (userAgent.includes("Edge")) {
+    return "Edge";
+  } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
+    return "Opera";
+  } else if (userAgent.includes("MSIE") || userAgent.includes("Trident/")) {
+    return "Internet Explorer";
+  } else {
+    return "Unknown Browser";
+  }
+}
+
+function getBrowserVersion() {
+  const userAgent = navigator.userAgent;
+  const versionStart = userAgent.indexOf(getBrowserName()) + getBrowserName().length + 1;
+  const versionEnd = userAgent.indexOf(" ", versionStart);
+  return userAgent.substring(versionStart, versionEnd);
+}
